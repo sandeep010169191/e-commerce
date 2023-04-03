b077ac9259f8afe67bdf6aa62458ae1d91f18086
sandeep010169191 <Sandeep.Chakraborty1@gds.ey.com>
2023-04-03 10:00:30 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home'
+import About from './components/About'
+import Product from './components/Product'
+import Contact from './components/Contact'
+//import { Redirect, Router, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Switch>
+        <Router to="/" component={Home} />
+        <Router to="/products" component={Product} />
+        <Router to="/about" component={About} />
+        <Router to="/contact" component={Contact} />
+        <Redirect to="/" />
+      </Switch> */}
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={Product} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Redirect path="/" />
+      </Switch>
+      <Footer />
+    </>
   );
 }
 
